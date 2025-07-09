9b9b78995621f53dd7c898dacd39946b8fd3d7cf
reginaKandekere <reginakandekere25@gmail.com>
2025-07-09 12:29:07 -0700

    removes comments from code

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
 
 let firstCard = Math.floor(Math.random() * (11-2+1))+2
 let secondCard = Math.floor(Math.random() * (11-2+1))+2
-
-// 2. Create a variable, sum, and set it to the sum of the two cards
 
 let sum = firstCard + secondCard
 
