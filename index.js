7f97813918699d1b9ed1e3cc133eefab1190f950
reginaKandekere <reginakandekere25@gmail.com>
2025-07-09 12:31:44 -0700

    adds background image, adds css file, adds buttons and messages to game display

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-
-let firstCard = Math.floor(Math.random() * (11-2+1))+2
-let secondCard = Math.floor(Math.random() * (11-2+1))+2
-
+let firstCard = 10
+let secondCard = 4
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
+function renderGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+
+function newCard() {
+    let card = 6
+    sum += card
+    renderGame()
+}
